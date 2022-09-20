03a20b0b3a8ada6bb0c426bffcf664836a3668eb
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-09-20 09:32:28 -0700

    print alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
-#program that prints the ASCII alphabet in lowercase.
 for a in range(ord('a'), ord('z') + 1):
-    print("{:c}".format(a), end = " ")
+    print("{:c}".format(a), end = '')
