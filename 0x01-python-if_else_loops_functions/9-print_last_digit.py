fc6b661d904fd6a313893e2c2ffde0c18da94bfc
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-09-20 21:52:32 -0700

    print last digit

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return("{:d}".format(number % 10))
+    if number < 0:
+        print("{:d}".format(-(number % -10)), end='')
+        return(-(number % -10))
+    else:
+        print("{:d}".format(number % 10), end='')
+        return(number % 10)
