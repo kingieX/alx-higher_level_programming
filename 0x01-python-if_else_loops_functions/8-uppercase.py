704fab693b0f556b202f513a443340360253e4fd
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-09-20 21:38:02 -0700

    uppercase

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord('a') <= ord(str) <= ord('z'):
+        if ord('a') <= ord(c) <= ord('z'):
             c = chr(ord(c) - (ord('a') - ord('A')))
         print("{:s}".format(c), end='')
     print("")
