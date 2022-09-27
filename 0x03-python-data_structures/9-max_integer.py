047e361d78688758037c120033a83c69b12d4be8
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-09-27 05:38:56 -0700

    max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def max_integer(my_list=[]):
     if len(my_list) == 0:
         return
@@ -9,4 +10,4 @@ def max_integer(my_list=[]):
             max_num = my_list[i]
         else:
             continue
-        return max_num
+    return max_num
