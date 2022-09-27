e484fd0c3c9141f1ac645d780075b954b7a3e645
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-09-26 21:26:18 -0700

    new in list

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_s = ''
-    for c in m_string:
+    new_s = ""
+    for c in my_string:
         if c != 'c' and c != 'C':
             new_s += c
     return new_s
