f702d199904c0aaa4f59f8bb257243881961ae25
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-10-12 15:49:34 -0700

    A Square that defines a Square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+class Square:
+    """"Represent a square
+
+    Attributes:
+        __size(int): size of a side of the square
+        """
+    def __init__(self,size):
+        """Initializes
+
+
+        Args:
+            size (int): size of a side of the square
+
+        Returns: None
+        """
+        self.size = size
