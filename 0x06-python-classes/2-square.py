56a29ab7572ef6be40735c7d8979310305c3b7b7
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-10-12 16:08:32 -0700

    A Square that defines a Square

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""defines a class square"""
+
+
+class Square:
+    """Represent a square
+
+    Arributes
+    __size (int): size of a sided of the square
+    """
+    def __init__(self, size=0):
+        """initializes the square
+        Args:
+            size (int): size of a side of the square
+        Returns:
+            None
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = size 
