e10d60be44d8aaedb795d15f849fc28d43fa44a9
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-10-18 05:17:38 -0700

    __new changes made

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -69,4 +69,3 @@ class Rectangle:
     def __repr__(self):
         """returns a string representation of the rectangle for reproduction"""
         return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-
