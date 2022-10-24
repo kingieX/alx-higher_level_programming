471e8134848c7ae007283f8ae9f932afc04a9b6b
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-10-24 10:12:46 -0700

    Base geometry with exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+empty base geometry
+"""
+
+
+class BaseGeometry:
+    """public attribute area"""
+    def area(self):
+    """raises an exception with a message"""
+    raise Exception("area() is not")
