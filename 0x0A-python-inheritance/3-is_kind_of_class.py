88cf5acfc7592489ce4b4f785c552593f9d68226
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-10-24 09:20:01 -0700

    is_kind

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""""
+contains Is_kind function
+"""
+
+def is_kind_of_class(obj, a_class):
+    """returns true if the objects is an instance of, otherwise false"""
+    return (isinstance(obj, a_class))
