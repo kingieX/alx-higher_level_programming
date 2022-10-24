65091eeecd0071f3338db4524a3aa5d637b71b64
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-10-24 09:53:35 -0700

    class inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,5 +5,5 @@ contain the inherited class
 
 
 def inherits_from(obj, a_class):
-    """true if the obj is an instance of a class that is inherited, else false"""
+    """true if obj is an instance of a class that is inherited, else false"""
     return(issubclass(type(obj), a_class) and type(obj) != a_class)
