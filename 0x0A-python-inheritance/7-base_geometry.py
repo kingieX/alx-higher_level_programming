5bbfca7f61f2f4ad3e670ebd83f16858d32203d2
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-10-25 08:21:09 -0700

    BaseGeometry

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+class BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """public attribute area"""
+    def area(self):
+        """raises an exception with a message"""
+        raise Exception("area() is not implemented")
+
+
+    def integer_validator(self, name, value):
+        """validates that value is an integer greater than 0"""
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
