9ebc3ef9d653a90c317922f63b53a79a8c1bbdcf
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-10-26 21:43:48 -0700

    pascal's triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -10,7 +10,7 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangels = [[1]]
+    triangles = [[1]]
     while len(triangles) != n:
         triangle = triangles[-1]
         tmp = [1]
