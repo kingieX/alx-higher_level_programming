5782ef8387d4aa441edb05e33078aeff826ec76b
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-10-26 20:44:56 -0700

    student class

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -6,6 +6,7 @@ class that defines a student
 
 class Student:
     """Representation of student class"""
+
     def __init__(self, first_name, last_name, age):
         """initialization of student"""
         self.first_name = first_name
@@ -15,9 +16,10 @@ class Student:
     def to_json(self, attrs=None):
         """retrieves a dictionary representation of a student instance
         if attrs is a list of strings, onlt attr names must be retrieved
-        otherwise all attr must be retrieved"""
+        otherwise all attr must be retrieved
+        """
         if (type(attrs) == list and
-                all(Type(ele) == str for ele in attrs)):
+                all(type(ele) == str for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
 
