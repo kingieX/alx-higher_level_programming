bd88a51120bd43bf61466140fd5af181f4e20832
Nnachi Chima <chimakingsley216@gmail.com>
2022-10-31 18:07:20 -0700

    models

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-This module contains the "Square" class
+Contains the "Square" class
 """
 from models.rectangle import Rectangle
 
@@ -10,17 +10,12 @@ class Square(Rectangle):
 
     def __init__(self, size, x=0, y=0, id=None):
         """Initialize a new Square.
-        Args:
-            size (int): The size of the new Square.
-            x (int): The x coordinate of the new Square.
-            y (int): The y coordinate of the new Square.
-            id (int): The identity of the new Square.
         """
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
-        """Get/set the size of the Square."""
+        """This set/get the size of the Square."""
         return self.width
 
     @size.setter
@@ -30,13 +25,6 @@ class Square(Rectangle):
 
     def update(self, *args, **kwargs):
         """Update the Square.
-        Args:
-            *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents size attribute
-                - 3rd argument represents x attribute
-                - 4th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
         """
         if args and len(args) != 0:
             a = 0
