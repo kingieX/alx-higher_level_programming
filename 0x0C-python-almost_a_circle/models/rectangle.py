c0a52ca5a44c451ebfb89e628ccbe7d1c7d1c865
Nnachi Chima <chimakingsley216@gmail.com>
2022-10-31 18:05:04 -0700

    models

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Defines a rectangle class."""
+"""Function that defines a rectangle class."""
 from models.base import Base
 
 
@@ -8,17 +8,12 @@ class Rectangle(Base):
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a new Rectangle.
-        Args:
+        Arguments:
             width (int): The width of the new Rectangle.
             height (int): The height of the new Rectangle.
             x (int): The x coordinate of the new Rectangle.
             y (int): The y coordinate of the new Rectangle.
             id (int): The identity of the new Rectangle.
-        Raises:
-            TypeError: If either of width or height is not an int.
-            ValueError: If either of width or height <= 0.
-            TypeError: If either of x or y is not an int.
-            ValueError: If either of x or y < 0.
         """
         self.width = width
         self.height = height
@@ -28,7 +23,7 @@ class Rectangle(Base):
 
     @property
     def width(self):
-        """Set/get the width of the Rectangle."""
+        """This set/get the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -41,7 +36,7 @@ class Rectangle(Base):
 
     @property
     def height(self):
-        """Set/get the height of the Rectangle."""
+        """This set/get the height of the Rectangle."""
         return self.__height
 
     @height.setter
@@ -54,7 +49,7 @@ class Rectangle(Base):
 
     @property
     def x(self):
-        """Set/get the x coordinate of the Rectangle."""
+        """This also set/get the x coordinate of the Rectangle."""
         return self.__x
 
     @x.setter
@@ -67,7 +62,7 @@ class Rectangle(Base):
 
     @property
     def y(self):
-        """Set/get the y coordinate of the Rectangle."""
+        """This set/get the y coordinate of the Rectangle."""
         return self.__y
 
     @y.setter
@@ -96,14 +91,6 @@ class Rectangle(Base):
 
     def update(self, *args, **kwargs):
         """Update the Rectangle.
-        Args:
-            *args (ints): New attribute values.
-                - 1st argument represents id attribute
-                - 2nd argument represents width attribute
-                - 3rd argument represent height attribute
-                - 4th argument represents x attribute
-                - 5th argument represents y attribute
-            **kwargs (dict): New key/value pairs of attributes.
         """
         if args and len(args) != 0:
             a = 0
