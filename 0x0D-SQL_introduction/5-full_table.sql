7cc45f1cd7864689d1e530fd80644735d5d15df5
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-11-30 04:59:03 -0800

    describe a table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,2 +1,2 @@
--- a script that prints the full description of the table.
-SHOW CREATE TABLE 'first_table';
+-- Prints the full description of the table first_table.
+SHOW CREATE TABLE `first_table`;
