5dfbba60e5f76f4f13389d485ae8a89116328631
Nnachi Chima Kingsley <chimakingsley216@gmail.com>
2022-12-01 02:39:32 -0800

    sql

---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,7 @@
+-- Lists all records of the table
+-- Results should display the score and the name (in this order)
+-- Records should be listed by descending score.
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
