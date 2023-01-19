987086a9abef1f54e295c544d53137afa5cbffd4
chimakingsley <chimakingsley216@gmail.com>
2023-01-19 08:40:32 -0800

    Python Network

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""takes in a URL and an email address, sends a POST request to the passed URL.
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {"email": sys.argv[2]}
+
+    r = requests.post(url, data=value)
+    print(r.text)
